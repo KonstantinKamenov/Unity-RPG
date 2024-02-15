2ec666ed32647fdcf9c154d1d5940698ebf6c8b9
KonstantinKamenov <tinikamenov@gmail.com>
2024-02-16 00:19:15 +0100

    Added player model and animation

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -7,10 +7,11 @@ public class Mover : MonoBehaviour
 {
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -22,5 +23,13 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float forwardSpeed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", forwardSpeed);
     }
 }
