d279bafc108c84833714bce32d4351a43430c1dc
KonstantinKamenov <tinikamenov@gmail.com>
2024-02-18 23:08:59 +0100

    Triggering death

--- Assets/Scrits/Combat/Health.cs
+++ Assets/Scrits/Combat/Health.cs
@@ -7,9 +7,23 @@ namespace RPG.Combat
     {
         [SerializeField] float health = 100f;
 
+        bool isDead = false;
+
         public void TakeDamage(float damage)
         {
             health = Math.Max(0f, health - damage);
+
+            if (health == 0)
+            {
+                Die();
+            }
+        }
+
+        public void Die()
+        {
+            if (isDead) return;
+            GetComponent<Animator>().SetTrigger("die");
+            isDead = true;
         }
     }
 }
