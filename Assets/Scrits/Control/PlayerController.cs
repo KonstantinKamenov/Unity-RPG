1122c9d978c979b8b62cbefd23b645f3402a48fe
KonstantinKamenov <tinikamenov@gmail.com>
2024-02-18 01:45:14 +0100

    Combat action priority

--- Assets/Scrits/Control/PlayerController.cs
+++ Assets/Scrits/Control/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
 
 namespace RPG.Control
 {
@@ -9,21 +10,46 @@ namespace RPG.Control
     {
         void Update()
         {
-            if (Input.GetMouseButton(0))
-            {
-                MoveToCursor();
-            }
+            if (InteractWithCombat()) return;
+            if (InteractWithMovement()) return;
         }
 
-        private void MoveToCursor()
+        private bool InteractWithCombat()
         {
-            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if (Input.GetMouseButton(0))
+                {
+                    GetComponent<Fighter>().Attack();
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private bool InteractWithMovement()
+        {
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(cameraRay, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
-                GetComponent<Mover>().MoveTo(hit.point);
+                if (Input.GetMouseButton(0))
+                {
+                    GetComponent<Mover>().MoveTo(hit.point);
+                }
+                return true;
             }
+            return false;
+        }
+
+        public static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
