14a4ce7974b08936e4b79f941f036c148e1cba51
KonstantinKamenov <tinikamenov@gmail.com>
2024-02-23 23:25:27 +0100

    Added cinematic sequence

---
+++ Assets/Scrits/Cinematics/CinematicsControlRemover.cs
@@ -0,0 +1,34 @@
+using RPG.Control;
+using RPG.Core;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicsControlRemover : MonoBehaviour
+    {
+        GameObject player;
+
+        private void Awake()
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+
+        private void OnEnable()
+        {
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+        }
+
+        public void DisableControl(PlayableDirector playableDirector)
+        {
+            player.GetComponent<ActionScheduler>().CancelAction();
+            player.GetComponent<PlayerController>().enabled = false;
+        }
+
+        public void EnableControl(PlayableDirector playableDirector)
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+    }
+}
