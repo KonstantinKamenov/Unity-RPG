9bf0c70de5371f30946b242129fec4e2538f1902
KonstantinKamenov <tinikamenov@gmail.com>
2024-05-09 09:47:46 +0200

    Melee weapon SFX

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] private UnityEvent onHit;
+
         public void OnHit()
         {
-            print(gameObject.name);
+            onHit.Invoke();
         }
     }
 }
